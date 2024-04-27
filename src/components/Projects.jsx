25e4cb41bce46f14919cdb2235fd32dfc8e9b805
unknown <okekekizito@gmail.com>
2024-04-27 13:07:39 +0100

    with updated projects without undertag

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,40 +1,24 @@
 import React from "react";
+import { Carousel } from "flowbite-react";
 import hairstylechangerai from "../../assets/hairstylechangerai.png";
 import quantumdoctor from "../../assets/quantumdoctor.png";
 import wikiapi from "../../assets/wikiapi.png";
 
 export default function Projects() {
   return (
-    <div className="mb-10 md:pt-14 pt-32" id="projects">
+    <div className="mb-10 md:pt-14 pt-32 md:h-screen" id="projects">
       <h2 className="text-center font-black mb-8">PROJECTS</h2>
-      <div className="flex w-full box-border h-screen pl-[7%] pr-[3%] pt-[4%] pb-[8%]">
-        <div className="hover:flex-[1_1_50%] flex-1 overflow-hidden transition-[0.5s] hover:duration-500 hover:ease-in-out shadow-[0_20px_30px_rgba(0,0,0,0.1)] leading-[0] mx-[2%] my-0">
-          <img
-            className="w-[200%] h-[calc(100%_-_10vh)] object-cover hover:w-full"
-            src={hairstylechangerai}
-            alt="hairstyle changer AI"
-          />
-          <span className="text-[3.8vh] block text-center h-[10vh] leading-[2.6]">Hairstyle Changer AI</span>
+ 
+        <div className="h-72 sm:h-64 md:h-96 lg:h-96 xl:h-5/6 2xl:h-96 md:w-5/6 flex items-center justify-center md:ml-28">
+        <Carousel>
+            <img src="../../assets/hairstylechangerai.png" alt="..." />
+            <img src="../../assets/quantumdoctor.png" alt="..." />
+            <img src="../../assets/wikiapi.png" alt="..." />
+            <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+            <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        </Carousel>
         </div>
 
-        <div className="hover:flex-[1_1_50%] flex-1 overflow-hidden transition-[0.5s] hover:duration-500 hover:ease-in-out shadow-[0_20px_30px_rgba(0,0,0,0.1)] leading-[0] mx-[2%] my-0">
-          <img
-            className="w-[200%] h-[calc(100%_-_10vh)] object-cover transition-[0.5s]"
-            src={quantumdoctor}
-            alt="QIT project"
-          />
-          <span className="text-[3.8vh] block text-center h-[10vh] leading-[2.6]">Quantum Doctor Web Application</span>
-        </div>
-
-        <div className="hover:flex-[1_1_50%] flex-1 overflow-hidden transition-[0.5s] hover:duration-500 hover:ease-in-out shadow-[0_20px_30px_rgba(0,0,0,0.1)] leading-[0] mx-[2%] my-0">
-          <img
-            className="w-[200%] h-[calc(100%_-_10vh)] object-cover transition-[0.5s] hover:w-full"
-            src={wikiapi}
-            alt="wiki API"
-          />
-          <span className="text-[3.8vh] block text-center h-[10vh] leading-[2.6]">Wiki-API</span>
-        </div>
-      </div>
     </div>
   );
 }
@@ -42,7 +26,6 @@ export default function Projects() {
 // https://source.unsplash.com/1000x800
 // https://source.unsplash.com/1000x802
 // https://source.unsplash.com/1000x804
-
 
 // https://i.postimg.cc/W3bTYcDd/Screenshot-12.png
 // https://i.postimg.cc/2y9yJMZy/Screenshot-14.png
